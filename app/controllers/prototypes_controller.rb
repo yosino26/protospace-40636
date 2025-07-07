8bf4ff4533f7d7ba16162671eac17b60fad9eae1
yosino26 <fn.yoshino57@gmail.com>
2025-07-07 21:58:57 +0900

    PrototypesControllerにedit と updateアクションを定義

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,6 +16,24 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+    # ログインユーザー以外が編集画面に来ないようにする場合
+    unless @prototype.user == current_user
+      redirect_to root_path, alert: "権限がありません。"
+    end
+  end
+  
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype), notice: "プロトタイプを更新しました！"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
