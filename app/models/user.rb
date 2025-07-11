a4709d98e6d3e1d60671cada28f288bf60545a79
yosino26 <fn.yoshino57@gmail.com>
2025-07-11 20:05:01 +0900

    Userモデルのバリテーションを新たに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true
+         validates :name, presence: true, length: { maximum: 15 }
+         validates :email, presence: true, uniqueness: true
+         validates :password, length: { minimum: 6 },
 
          has_many :comments, dependent: :destroy
          has_many :prototypes, dependent: :destroy
