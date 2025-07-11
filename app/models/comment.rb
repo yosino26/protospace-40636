a82a10952001573a0771367074408112e0f6bc30
yosino26 <fn.yoshino57@gmail.com>
2025-07-12 02:03:25 +0900

    commentモデルのバリテーションを新たに設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :prototype
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 300 }
 end
